8f58174376fffbd36d5462f83956de0feee9e481
Muhammad Al-Giffari <gif.glses@gmail.com>
2026-05-03 22:01:04 +0700

    first init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-red-500 font-bold text-4xl text-center m-auto'>Hi, ini adalah web admin!</h1>
+    </>
+  )
+}
+
+export default App
